f13e894a3ca64bfac03a06790ff6c381091cf1c6
Txuritan <txuritan@protonmail.com>
2019-02-28 15:31:20 -0500

    Added backend note

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,3 +1,6 @@
+// This module is just ripped versions of the r2d2 implementations.
+// This allows me to manually update the library without waiting for the author.
+
 pub mod mysql;
 pub mod postgres;
 pub mod sqlite;
